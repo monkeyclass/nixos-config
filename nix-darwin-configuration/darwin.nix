4fb295e30ab738e227e7df41bcbad733dfe1cc2c
monkeyclass <oliver+github@kasoli.eu>
2024-05-08 23:08:04 +0200

    initial commit

---
+++ nix-darwin-configuration/darwin.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, ... }:
+
+let
+  user = "oliver";
+in 
+{
+  users.users.${user}.home = "/Users/${user}";
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+      pkgs.vim pkgs.vscodium pkgs.git pkgs.legendary-gl pkgs.fish pkgs.joplin-desktop
+    ];
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "uninstall";
+
+    taps = [];
+    brews = [];
+    casks = [
+      "firefox" "nextcloud"
+    ];
+  };
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+  
+  # intel binaries
+  # remember to manually run softwareupdate --install-rosetta --agree-to-license first
+  nix.extraOptions = ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # fingerprint sudo
+  security.pam.enableSudoTouchIdAuth = true;
+
+  system.defaults = {
+    dock = {
+      autohide =true;
+      mru-spaces = false;
+      persistent-apps = [
+        "/System/Applications/Launchpad.app/"
+        "/Applications/Firefox.app"
+        "/System/Applications/Utilities/Terminal.app"
+        "${pkgs.vscodium}/Applications/VSCodium.app"
+        "${pkgs.joplin-desktop}/Applications/Joplin.app"
+        "/System/Applications/Messages.app/"
+        "/System/Applications/Facetime.app/"
+      ];
+    };
+    finder.AppleShowAllExtensions = true;
+    finder.AppleShowAllFiles = true;
+    finder.FXPreferredViewStyle = "clmv";
+    screencapture.location = "~/Pictures/screenshots";
+    screensaver.askForPasswordDelay = 10;
+  };
+}
---
+++ nix-darwin-configuration/flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+
+    nur = {
+      url = "github:nix-community/NUR";
+    };
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, ... }:
+  let
+    user = "oliver";
+    hostname = "Olivers-MacBook-Air";
+  in 
+  {
+    darwinConfigurations = {
+      "${hostname}" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin.nix
+          {
+            nixpkgs.overlays = [
+              inputs.nur.overlay
+            ];
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${user} = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "oliver";
+
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+              };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
