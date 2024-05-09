99b138678f88d09e4acde72f197491d913b4d249
monkeyclass <30174727+monkeyclass@users.noreply.github.com>
2024-05-09 11:01:36 +0200

    update firefox config

--- nix-darwin-configuration/home.nix
+++ nix-darwin-configuration/home.nix
@@ -80,9 +80,43 @@ in
         id = 0;
         name = "default";
         isDefault = true;
+        settings = {
+          "signon.rememberSignons" = false;
+          "widget.disable-workspace-management" = true;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.compactmode.show" = true;
+        };
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = [ "DuckDuckGo" "Google" ];
+          engines = {
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Wiki" = {
+              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+            "Bing".metaData.hidden = true;
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+        };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           bitwarden
+          privacy-badger
+          darkreader
         ];
       };
     };
